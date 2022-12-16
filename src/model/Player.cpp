2d2c4b8e9cf0ba7a039cb762155010ff3f9db793
AchrafYndz <achraf.yandouzi@gmail.com>
2022-12-16 22:01:18 +0100

    Fix jittery player

--- src/model/Player.cpp
+++ src/model/Player.cpp
@@ -69,108 +69,106 @@ void Player::processInput() {
 }
 
 void Player::update() {
-    processInput();
+	processInput();
 
 	Vec2 plyPos = Camera::getInstance()->toPixels(getPosition()); // top left corner
 
-    int currentY = plyPos.y;
-    if (currentJumpingTime > 0) {
-		if (jumpType == JumpType::normal || jumpType == JumpType::toTheLeft || jumpType == JumpType::toTheRight) {
-			sprite.setPosition(plyPos.x, currentY - topSpeed);
-			position = Camera::getInstance()->normalizedPosition(Vec2(sprite.getPosition().x, sprite.getPosition().y));
-		}
+	int currentY = plyPos.y;
+	if (currentJumpingTime > 0) {
+		if (jumpType == JumpType::normal || jumpType == JumpType::toTheLeft || jumpType == JumpType::toTheRight)
+			plyPos.y -= topSpeed;
 
-        // if one third of the jump -> deceleration
-        if (currentJumpingTime < JUMPING_TOTAL_TIME / 3.f) {
-            sprite.setPosition(plyPos.x, currentY + jumpDeceleration);
-			position = Camera::getInstance()->normalizedPosition(Vec2(sprite.getPosition().x, sprite.getPosition().y));
-        }
+		// if one third of the jump -> deceleration
+		if (currentJumpingTime < JUMPING_TOTAL_TIME / 3.f) plyPos.y += jumpDeceleration;
 
-        currentJumpingTime -= TIME_PER_FRAME;
-    } else { // apply gravity
-        sprite.setPosition(plyPos.x, currentY + topSpeed);
-		position = Camera::getInstance()->normalizedPosition(Vec2(sprite.getPosition().x, sprite.getPosition().y));
-    }
+		currentJumpingTime -= TIME_PER_FRAME;
 
-    /// collision
-	
-    // update sensor positions:
-    float sensorOffset = 4.f;
-    leftSensor.x = plyPos.x - sensorOffset;
-    leftSensor.y = plyPos.y + (SCALE * sprite.getLocalBounds().height / 4.f);
-    leftSensor.active = false;
-    rightSensor.x = plyPos.x + (SCALE * sprite.getLocalBounds().width) + sensorOffset;
-    rightSensor.y = plyPos.y + (SCALE * sprite.getLocalBounds().height / 4.f);
-    rightSensor.active = false;
-    bottomSensor.x = plyPos.x + (SCALE * sprite.getLocalBounds().width / 2.f);
-    bottomSensor.y = plyPos.y + (SCALE * sprite.getLocalBounds().height) + sensorOffset;
-    bottomSensor.active = false;
+	}
+	else  plyPos.y += topSpeed; // apply gravity
 
-    Vec2 ePos;
-    sf::FloatRect r;
-    for (auto e : World::getInstance()->getEntities()) {
-        if (e->getType() == Type::wall) {
+								/// collision
+
+								// update sensor positions:
+	float sensorOffset = 4.f;
+	leftSensor.x = plyPos.x - sensorOffset;
+	leftSensor.y = plyPos.y + (SCALE * sprite.getLocalBounds().height / 4.f);
+	leftSensor.active = false;
+	rightSensor.x = plyPos.x + (SCALE * sprite.getLocalBounds().width) + sensorOffset;
+	rightSensor.y = plyPos.y + (SCALE * sprite.getLocalBounds().height / 4.f);
+	rightSensor.active = false;
+	bottomSensor.x = plyPos.x + (SCALE * sprite.getLocalBounds().width / 2.f);
+	bottomSensor.y = plyPos.y + (SCALE * sprite.getLocalBounds().height) + sensorOffset;
+	bottomSensor.active = false;
+
+	Vec2 ePos;
+	sf::FloatRect r;
+	for (auto e : World::getInstance()->getEntities()) {
+		if (e->getType() == Type::wall) {
 			Vec2 ePos = Camera::getInstance()->toPixels(e->getPosition()); // top left corner
 
-            r.left = ePos.x;
-            r.top = ePos.y;
-            r.height = TILESIZE * SCALE;
-            r.width = TILESIZE * SCALE;
+			r.left = ePos.x;
+			r.top = ePos.y;
+			r.height = TILESIZE * SCALE;
+			r.width = TILESIZE * SCALE;
 
-            if (r.contains(bottomSensor.x, bottomSensor.y)) {
-                bottomSensor.active = true;
-//				std::cout << "bottom active";
-            } else if (r.contains(leftSensor.x, leftSensor.y)) {
-                leftSensor.active = true;
-//				std::cout << "left active";
-            } else if (r.contains(rightSensor.x, rightSensor.y)) {
-                rightSensor.active = true;
-//				std::cout << "right active";
-            }
-        }
+			if (r.contains(bottomSensor.x, bottomSensor.y)) {
+				bottomSensor.active = true;
+			}
+			else if (r.contains(leftSensor.x, leftSensor.y)) {
+				leftSensor.active = true;
+			}
+			else if (r.contains(rightSensor.x, rightSensor.y)) {
+				rightSensor.active = true;
+			}
+		}
 
-    }
-    if (bottomSensor.active)
-        state = PlyState::standingOnTile;
-    else if (leftSensor.active)
-        state = PlyState::onLeftWall;
-    else if (rightSensor.active)
-        state = PlyState::onRightWall;
-    else
-        state = PlyState::onAir;
+	}
+	if (bottomSensor.active)
+		state = PlyState::standingOnTile;
+	else if (leftSensor.active)
+		state = PlyState::onLeftWall;
+	else if (rightSensor.active)
+		state = PlyState::onRightWall;
+	else
+		state = PlyState::onAir;
 
-    for (auto e : World::getInstance()->getEntities()) {
-        // player with wall
-        if (e->getType() == Type::wall) {
-            Vec2 overlapPly = World::getInstance()->getOverlap(plyPos, Camera::getInstance()->toPixels(e->getPosition()));
-            bool thereIsCollision = overlapPly.x > 0.f && overlapPly.y > 0.f;
+	for (auto e : World::getInstance()->getEntities()) {
+		// player with wall
+		if (e->getType() == Type::wall) {
+			Vec2 overlapPly = World::getInstance()->getOverlap(plyPos, Camera::getInstance()->toPixels(e->getPosition()));
+			bool thereIsCollision = overlapPly.x > 0.f && overlapPly.y > 0.f;
 
-            if (thereIsCollision) {
-                if (overlapPly.y < overlapPly.x) { // compensate only the shortest overlap, in this case y
-                    if (bottomSensor.active) {     // player on top of tile
-                        plyPos.y -= overlapPly.y;
-                    } else { // player hit tile from below
-                        plyPos.y += overlapPly.y;
-                    }
-                } else {                      // compensate only x
-                    if (rightSensor.active) { // player colliding on right wall
-                        plyPos.x -= overlapPly.x;
-                    } else if (leftSensor.active) { // player colliding on left wall
-                        plyPos.x += overlapPly.x;
-                    }
-                }
-                sprite.setPosition(plyPos.x, plyPos.y);
-				position = Camera::getInstance()->normalizedPosition(Vec2(sprite.getPosition().x, sprite.getPosition().y));
-            }
-        }
-        // player with goal (bandage girl)
-        else if (e->getType() == Type::goal) {
-            Vec2 overlapPly = World::getInstance()->getOverlap(plyPos, Camera::getInstance()->toPixels(e->getPosition()));
-            bool thereIsCollision = overlapPly.x > 0.f && overlapPly.y > 0.f;
-            if (thereIsCollision)
-                reachedGoal = true;
-        }
-    }
+			if (thereIsCollision) {
+				if (overlapPly.y < overlapPly.x) { // compensate only the shortest overlap, in this case y
+					if (bottomSensor.active) {     // player on top of tile
+						plyPos.y -= overlapPly.y;
+					}
+					else { // player hit tile from below
+						plyPos.y += overlapPly.y;
+					}
+				}
+				else { // compensate only x
+					if (rightSensor.active) { // player colliding on right wall
+						plyPos.x -= overlapPly.x;
+					}
+					else if (leftSensor.active) { // player colliding on left wall
+						plyPos.x += overlapPly.x;
+					}
+				}
+
+			}
+		}
+		// player with goal (bandage girl)
+		else if (e->getType() == Type::goal) {
+			Vec2 overlapPly = World::getInstance()->getOverlap(plyPos, Camera::getInstance()->toPixels(e->getPosition()));
+			bool thereIsCollision = overlapPly.x > 0.f && overlapPly.y > 0.f;
+			if (thereIsCollision)
+				reachedGoal = true;
+		}
+	}
+
+	sprite.setPosition(plyPos.x, plyPos.y);
+	position = Camera::getInstance()->normalizedPosition(Vec2(sprite.getPosition().x, sprite.getPosition().y));
 }
 
 void Player::draw() {
